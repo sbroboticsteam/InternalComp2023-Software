a009bd6eaa1a0c7e111e6642a87dd81581b495b5
Shrujan <shrujanus@gmail.com>
2023-11-10 22:06:19 -0500

    updated sockets

--- IC_Example_Code/wasd_ws/sockets/sockets.ino
+++ IC_Example_Code/wasd_ws/sockets/sockets.ino
@@ -1,25 +1,7 @@
 #include <WiFi.h>
 #include <WebSocketsServer.h>
 #include <ESPAsyncWebServer.h>
-#include <esp_camera.h>
 
-
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    21
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      19
-#define Y4_GPIO_NUM      18
-#define Y3_GPIO_NUM       5
-#define Y2_GPIO_NUM       4
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
 
 const char* ssid = "SBRT";
 const char* password = "Robotic$3";
@@ -187,45 +169,6 @@ void setup() {
   Serial.setDebugOutput(true);
   Serial.println();
 
-   camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-    if (psramFound()) {
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
-
-    esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-  sensor_t * s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_SVGA);
-
   WiFi.begin(ssid, password);
   WiFi.setSleep(false);
 
@@ -253,22 +196,6 @@ server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
 
 void loop() {
   webSocket.loop();
-
-  if(webSocket.connectedClients()) {
-    //Serial.println("WebSocket client is connected. Attempting to get camera frame...");
-    camera_fb_t * fb = esp_camera_fb_get();
-    if(fb) {
-      //Serial.println("Camera frame captured, sending over WebSocket...");
-      webSocket.broadcastBIN(fb->buf, fb->len);
-      esp_camera_fb_return(fb);
-    } else {
-      //Serial.println("Failed to capture camera frame!");
-    }
-  } else {
-    //                                                                                                              Serial.println("No WebSocket clients connected.");
-  }
-
-  delay(10); // A short delay to prevent excessive printing to the serial monitor
 }
 
 
